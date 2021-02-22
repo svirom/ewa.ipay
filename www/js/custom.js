7107d9cc88492bb8be6bfe8270ff1a389f81f0eb
svirom <svirom@yahoo.com>
2021-02-22 19:22:32 +0200

    header

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-  
+  // prevent close dropdown menu after notification card or support card close (header notification, support)
+  $(document).on('click', '.main-nav__notification .dropdown-menu, .main-nav__support .dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
+
+  // close support card (header support)
+  $('.main-nav__support .support-card .close').click(function() {
+    $('#dropdown-support').dropdown('toggle');
+  })
 
 })
